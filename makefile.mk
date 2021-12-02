1ba8b81a78578b44b688e6031eec1596d0e26e2b
CollinNMcGee <54588639+CollinNMcGee@users.noreply.github.com>
2021-12-02 17:45:47 -0500

    added cppcheck

--- makefile.mk
+++ makefile.mk
@@ -1,3 +1,7 @@
 g++ -g -wall -o count  countLine.h countChar.h main.cpp
 docs:
 	doxygen ./Doxyfile
+cppcheck.out.xml: main.cpp countLine.h countChar.h
+	cppcheck -q --error-exitcode=1 countLine.h countChar.h main.cpp
+valgrind:
+	valgrind countLine.h countChar.h main.cpp
